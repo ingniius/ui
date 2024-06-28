7b7ac2edf9bbf35917bdeea8499b458371bd5e00
Mau Costa <omaucf@gmail.com>
2024-06-28 13:26:55 -0300

    feat(util): add types > DeepMerge, DeepPartial :label:

--- packages/core/util/src/types.ts
+++ packages/core/util/src/types.ts
@@ -4,9 +4,47 @@
 type Key = string | number | symbol;
 
 /**
+ * @private
+ */
+type Intersect<T, U> = Omit<T | U, keyof Subtract<T, U>>;
+
+/**
+ * @private
+ */
+type Subtract<
+  T,
+  U,
+  X = Omit<T, keyof U> & Omit<U, keyof T>,
+  Y = {
+    [K in keyof X]: X[K];
+  },
+> = Y;
+
+/**
  * @publicApi
  */
-export type Dict<T extends Key = Key, V = any> = Record<T, V>;
+export type DeepMerge<
+  T,
+  U,
+  // non shared keys are optional
+  X = Partial<Subtract<T, U>> & {
+    // shared keys are required
+    [K in keyof Intersect<T, U>]: T[K] | U[K];
+  },
+  Y = { [K in keyof X]: X[K] },
+> = Y;
+
+/**
+ * @publicApi
+ */
+export type DeepPartial<T> = T extends Dict
+  ? { [P in keyof T]?: DeepPartial<T[P]> }
+  : T;
+
+/**
+ * @publicApi
+ */
+export type Dict<K extends Key = Key, V = any> = Record<K, V>;
 
 /**
  * @publicApi
