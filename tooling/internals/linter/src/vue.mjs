cd06845a008fa9895bc45e51b33a128561190113
Mau Costa <omaucf@gmail.com>
2024-06-28 13:14:01 -0300

    feat(linter): add base > vue :wrench:

--- tooling/internals/linter/src/vue.mjs
+++ tooling/internals/linter/src/vue.mjs
@@ -3,12 +3,17 @@ import { createConfigForNuxt } from "@nuxt/eslint-config/flat";
 import eslintConfigPrettier from "eslint-config-prettier";
 import tailwind from "eslint-plugin-tailwindcss";
 
-import { settings } from "./base.mjs";
-import { rules } from "./vue.mjs";
+import { rules as base, settings } from "./base.mjs";
 
-export function defineConfig(options, ...configs) {
+export const rules = {
+  ...base,
+  "vue/multi-word-component-names": "off",
+  "vue/no-multiple-template-root": "off",
+};
+
+export function defineConfig(...configs) {
   return createConfigForNuxt(
-    options ?? {},
+    {},
     eslintConfigPrettier,
     ...tailwind.configs["flat/recommended"],
     ...(configs ?? []),
