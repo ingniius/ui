12f73124744ed92279b95d56407f50bf59985f3a
Mau Costa <omaucf@gmail.com>
2024-11-04 11:48:38 -0300

    chore: add prettier :package:

--- turbo/generators/package/generator.ts
+++ turbo/generators/package/generator.ts
@@ -16,14 +16,12 @@ export function createPackageGenerator(plop: PlopTypes.NodePlopAPI) {
       {
         type: "input",
         name: "name",
-        message:
-          "What is the name of the package? (You can skip the `@vee-ui/` prefix)",
+        message: "What is the name of the package? (You can skip the `@vee-ui/` prefix)",
       },
       {
         type: "input",
         name: "deps",
-        message:
-          "Enter a space separated list of dependencies you would like to install",
+        message: "Enter a space separated list of dependencies you would like to install",
       },
     ],
     actions: [
@@ -303,9 +301,7 @@ export function isBrowser() {
           if ("deps" in answers && typeof answers.deps === "string") {
             const pkg = JSON.parse(content) as PackageJson;
             for (const dep of answers.deps.split(" ").filter(Boolean)) {
-              const version = await fetch(
-                `https://registry.npmjs.org/-/package/${dep}/dist-tags`,
-              )
+              const version = await fetch(`https://registry.npmjs.org/-/package/${dep}/dist-tags`)
                 .then((res) => res.json())
                 .then((json) => json.latest);
               if (!pkg.dependencies) pkg.dependencies = {};
@@ -320,9 +316,7 @@ export function isBrowser() {
       async (answers) => {
         execSync("pnpm exec manypkg fix", { stdio: "inherit" });
         execSync(
-          `pnpm exec prettier --write packages/${
-            (answers as { name: string }).name
-          }/** --list-different`,
+          `pnpm exec prettier --write packages/${(answers as { name: string }).name}/** --list-different`,
         );
         return "Package scaffolded";
       },
