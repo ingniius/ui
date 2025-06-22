9c47d5fb43e90121e22033cefb93943b609af7ec
omaucf <omaucf@protonmail.com>
2025-06-22 07:29:31 -0300

    chore: initial commit :tada:

---
+++ src/UI.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vee;
+
+use Illuminate\Support\Facades\Facade;
+
+final class UI extends Facade
+{
+    public static function getFacadeAccessor(): string
+    {
+        return 'vee';
+    }
+}
